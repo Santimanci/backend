6fe17c3e9db01187faf6ffb20ee1ed3bda63c974
camilo2501roco <aparendizcamilo@gmail.com>
2026-03-04 21:58:06 -0500

    manejo de envios de correos

--- helpers/recuperacion.js
+++ helpers/recuperacion.js
@@ -17,11 +17,16 @@ export const generarCodigo = () => {
  */
 const crearTransporte = () => {
   return nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
+    tls: {
+      rejectUnauthorized: false
+    }
   });
 };
 
